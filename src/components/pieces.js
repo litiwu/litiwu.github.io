31e107553c7a94b7dc8f694d478a86b2fdb8e87e
Lisa Wu <lisawu0119@gmail.com>
2021-03-15 19:19:26 +0800

    adding src

---
+++ src/components/pieces.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Piece = () => {
+    return (
+        <div className="piece-wrapper">
+            <div className="Img">
+
+            </div>
+        </div>
+        
+    )
+}
+
+const Img = ({title, caption}) => {
+
+}
